1533a085886766b09b94da18d4e880a20930ec98
yubingjiaocn <yubingjiaocn@gmail.com>
2023-11-17 23:43:23 +0800

    Use submodule to create multiple clusters

--- cluster/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" {
-  description = "Region to deploy the resources"
-  type        = string
-}
-
-variable "name" {
-  description = "Name of created cluster"
-  type        = string
-}
